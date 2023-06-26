b9018dceb6a6538ab2c79a882dbbf01711f8db67
Deepanshu380 <deepanshua380@gmail.com>
2023-06-26 16:50:49 +0530

    Signup and Auth for Google added feature Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import './App.css';
 
 import { Routes, Route } from "react-router-dom";
-// import SignUpComp from './components/SignUpComp';
+import SignUpComp from './components/SignUpComp';
 import Landingpage from './components/Landingpage';
 import { Linesdata } from './components/Linesdata';
 // import ResetPassword from './components/Reset Password/ResetPassword';
-// import  LoginPage from './components/Login/LoginPage';
+import  LoginPage from './components/Login/LoginPage';
 import About from './components/About/About';
 
 function App() {
   return (
     <>
       <Routes>
-        {/* <Route path="/SignUpComp" element={<SignUpComp />} /> */}
-        {/* <Route path="/login" element={<LoginPage />} />
-        <Route path="/reset" element={<ResetPassword />} /> */}
+        <Route path="/SignUpComp" element={<SignUpComp />} />
+         <Route path="/login" element={<LoginPage />} />
+        {/* <Route path="/reset" element={<ResetPassword />} />  */}
         <Route path="/:name/line" element={<Linesdata />} />
         <Route path="/" element={<Landingpage />} />
         <Route path="/aboutUs" element={<About />} />
