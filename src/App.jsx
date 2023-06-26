9b01fc012ed3ab870e855be194a11e66ce872dd9
Deepanshu380 <deepanshua380@gmail.com>
2023-06-26 16:53:30 +0530

    Reset Password feature Added

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import SignUpComp from './components/SignUpComp';
 import Landingpage from './components/Landingpage';
 import { Linesdata } from './components/Linesdata';
-// import ResetPassword from './components/Reset Password/ResetPassword';
+import ResetPassword from './components/Reset Password/ResetPassword';
 import  LoginPage from './components/Login/LoginPage';
 import About from './components/About/About';
 
@@ -14,7 +14,7 @@ function App() {
       <Routes>
         <Route path="/SignUpComp" element={<SignUpComp />} />
          <Route path="/login" element={<LoginPage />} />
-        {/* <Route path="/reset" element={<ResetPassword />} />  */}
+        <Route path="/reset" element={<ResetPassword />} /> 
         <Route path="/:name/line" element={<Linesdata />} />
         <Route path="/" element={<Landingpage />} />
         <Route path="/aboutUs" element={<About />} />
